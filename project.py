e18e8418483f7f62aa8cb0eea17eabf56acae751
TheWolfCraft1 <wolfcraftmcg2@gmail.com>
2026-01-17 15:14:39 -0500

    cant be bothered thinking of a commit message

--- project.py
+++ project.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python
 print("Hello World")
-#https://prod.liveshare.vsengsaas.visualstudio.com/join?FF4D36C0A8CD445990B90065F6F07C7A0857
+#https://prod.liveshare.vsengsaas.visualstudio.com/join?894EAD2625D3B88D84FDB096F10CAD6280CA
+
+'''
+Ideas
+1. 
+2. 
+3. 
+'''
